4337b9e1b3e2d8c06df0e3bc7e658133bf3acfba
sourav kumar <souravk326@gmail.com>
2022-08-13 15:31:12 +0530

    python file added

---
+++ word_cloud.py
@@ -0,0 +1,71 @@
+# Here are all the installs and imports you will need for your word cloud script and uploader widget
+#CELL 1
+!pip install wordcloud
+!pip install fileupload
+!pip install ipywidgets
+!jupyter nbextension install --py --user fileupload
+!jupyter nbextension enable --py fileupload
+
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+#CELL 2
+# This is the uploader widget
+def _upload():
+
+    _upload_widget = fileupload.FileUploadWidget()
+
+    def _cb(change):
+        global file_contents
+        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+
+    _upload_widget.observe(_cb, names='data')
+    display(_upload_widget)
+
+_upload()
+
+#CELL 3
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+                           "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its",
+                           "they", "them", \
+                           "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be",
+                           "been", "being", \
+                           "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when",
+                           "where", "how", \
+                           "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very",
+                           "can", "will", "just"]
+
+    for s in file_contents:
+        if s in punctuations:
+            file_contents = file_contents.replace(s, "")
+    files = file_contents.lower().split()
+    result = {}
+    for word in files:
+        if word.isalpha() == True and word not in uninteresting_words:
+            if word in result:
+                result[word] += 1
+            else:
+                result[word] = 1
+
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(result)
+    return cloud.to_array()
+
+#CELL 4
+
+myimage = calculate_frequencies(file_contents)
+plt.imshow(myimage, interpolation = 'nearest')
+plt.axis('off')
+plt.show()
